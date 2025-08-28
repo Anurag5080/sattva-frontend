fb11069c914053c3260d7d72e19a15c70da5d9f4
Anurag Singh <anurag26official@gmail.com>
2025-08-28 10:34:49 +0530

    Build eslint errors fixed

--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import FeaturedCard from '../Components/FeaturedCard'
 import Image from 'next/image'
 
 const Collection = () => {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import Footer from "@/app/Components/Footer"
-import Footer2 from "@/app/Components/Footer2"
-import Header from "@/app/Components/Header"
-import About from "@/app/about/page"
 import Homepage from "./homepage/page"
 
 const page = () => {
